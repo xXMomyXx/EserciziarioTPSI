074e3d71c14a82f238c97f4485b4c255b90c8040
xXMomyXx <150335339+xXMomyXx@users.noreply.github.com>
2026-02-03 23:14:38 +0100

    Lato client di versione da completare monitoraggio ambientale

---
+++ SocketPython/MonitoraggioAmbientale/client.py
@@ -0,0 +1,89 @@
+import socket
+import tkinter as tk
+from tkinter import messagebox
+from datetime import datetime
+
+# Inizializzazione lato client della socket su porta 12345
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('localhost', 12345))
+global nGiorni, daysData
+
+
+def getNumDays():
+    global nGiorni
+    try:
+        giorni = int(entry.get())
+        if giorni < 0:
+            raise ValueError("Il numero di giorni deve essere maggiore di zero")
+    except ValueError:
+        messagebox.showinfo("Errore", "Il numero di giorni deve essere in formato numerico e maggiore di zero")
+        return
+    nGiorni = giorni
+    start_window.destroy()
+    root.deiconify()
+
+
+# Funzione di inserimento per mandare richieste al server
+def mainInterfaceLogic():
+    # Inserimento città di partenza e di arrivo
+    giorno = entry1.get()
+    try:
+        datetime.strptime(giorno, "%d/%m/%Y")
+    except ValueError:
+        messagebox.showinfo("Errore", "Formato non valido. Usa GG/MM/AAAA")
+        return
+    try:
+        temp12 = int(entry2.get())
+        temp24 = int(entry3.get())
+    except ValueError:
+        messagebox.showinfo("Errore", "Le temperature devono essere in formato numerico")
+        return
+    # Unione di città di partenza e arrivo in un unica stringa separate da '|'
+    message = f"{giorno}|{temp12}|{temp24}"
+
+
+def sendData(message):
+    # Invio della stringa sotto forma di sequenza di byte (encode())
+    client_socket.sendall(message.encode())
+    # Ricezione risposta server e decodifica della sequenza di byte (decode())
+    response = client_socket.recv(1024).decode()
+    # Presentazione su schermo della risposta ricevuta
+    messagebox.showinfo("Risposta del server", response)
+    # Finita la disponibilità di taxi si chiude il lato client della socket
+    if response:
+        client_socket.close()
+        root.destroy()
+
+
+# Chiamata della funzione 'start_client' in main attraverso il 'button' di Tkinter
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+    start_window = tk.Toplevel(root)
+    start_window.title("Invio Temperature Giornaliere – Stazione Meteo")
+    label = tk.Label(start_window, text="Per quanti giorni vuoi inserire i dati?")
+    label.pack(pady=5)
+    entry = tk.Entry(start_window)
+    entry.pack(pady=5)
+    button = tk.Button(start_window, text="Conferma", command=getNumDays)
+    button.pack(pady=10)
+    start_window.geometry("500x100")
+    root.geometry("300x250")
+    label1 = tk.Label(root, text="Inserisci il giorno di rilevazione (GG/MM/AAAA)")
+    label1.pack(pady=5)
+    entry1 = tk.Entry(root)
+    entry1.pack(pady=5)
+    label2 = tk.Label(root, text="Inserisci la temperatura rilevata alle ore 12:00")
+    label2.pack(pady=5)
+    entry2 = tk.Entry(root)
+    entry2.pack(pady=5)
+    label3 = tk.Label(root, text="Inserisci la temperatura rilevata alle ore 24:00")
+    label3.pack(pady=5)
+    entry3 = tk.Entry(root)
+    entry3.pack(pady=5)
+    button1 = tk.Button(root, text="Invia dati", command=mainInterfaceLogic)
+    button1.pack(pady=10)
+    root.mainloop()
+
+# Da aggiungere funzionamento con for loop per visualizzare la GUI il numero
+# di giorni necessario e salvare i dati formattati come message nella lista daysData(ancora da dichiarare come lista)
