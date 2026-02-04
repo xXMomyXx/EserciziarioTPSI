c986727778083ee9253bc07c0cf092ef0d7eed60
xXMomyXx <150335339+xXMomyXx@users.noreply.github.com>
2026-02-04 12:25:53 +0100

    Reso codice più compatto e aggiunta nuova logica e GUI

--- SocketPython/MonitoraggioAmbientale/client.py
+++ SocketPython/MonitoraggioAmbientale/client.py
@@ -6,14 +6,15 @@ from datetime import datetime
 # Inizializzazione lato client della socket su porta 12345
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost', 12345))
-global nGiorni, daysData
+nGiorni, counter = 0, 0
+daysData = []
 
 
-def getNumDays():
+def getNumDays(start_window, entry):
     global nGiorni
     try:
         giorni = int(entry.get())
-        if giorni < 0:
+        if giorni <= 0:
             raise ValueError("Il numero di giorni deve essere maggiore di zero")
     except ValueError:
         messagebox.showinfo("Errore", "Il numero di giorni deve essere in formato numerico e maggiore di zero")
@@ -23,8 +24,16 @@ def getNumDays():
     root.deiconify()
 
 
+# Funzione che itera per ogni elemento all'interno di una finestra e se sono di tipo 'Entry' li svuota
+def clearEntries(window):
+    for widget in window.winfo_children():
+        if isinstance(widget, tk.Entry):
+            widget.delete(0, 'end')
+
+
 # Funzione di inserimento per mandare richieste al server
-def mainInterfaceLogic():
+def mainInterfaceLogic(entry1, entry2, entry3):
+    global daysData, counter
     # Inserimento città di partenza e di arrivo
     giorno = entry1.get()
     try:
@@ -39,12 +48,18 @@ def mainInterfaceLogic():
         messagebox.showinfo("Errore", "Le temperature devono essere in formato numerico")
         return
     # Unione di città di partenza e arrivo in un unica stringa separate da '|'
-    message = f"{giorno}|{temp12}|{temp24}"
+    message = f"{giorno}|{temp12}|{temp24}."
+    daysData.append(message)
+    counter += 1
+    # Operatore ternario: Finito l'inserimento dei giorni la finestra viene chiusa, altrimenti si svuotano gli spazi di
+    # inserimento in preparazione del prossimo inserimento
+    root.withdraw(), sendData(daysData), result_window.deiconify if counter == nGiorni else clearEntries(root)
 
 
-def sendData(message):
-    # Invio della stringa sotto forma di sequenza di byte (encode())
-    client_socket.sendall(message.encode())
+def sendData(data):
+    for message in data:
+        # Invio della stringa sotto forma di sequenza di byte (encode())
+        client_socket.sendall(message.encode())
     # Ricezione risposta server e decodifica della sequenza di byte (decode())
     response = client_socket.recv(1024).decode()
     # Presentazione su schermo della risposta ricevuta
@@ -52,22 +67,21 @@ def sendData(message):
     # Finita la disponibilità di taxi si chiude il lato client della socket
     if response:
         client_socket.close()
-        root.destroy()
 
 
-# Chiamata della funzione 'start_client' in main attraverso il 'button' di Tkinter
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.withdraw()
+def startWindowInit():
     start_window = tk.Toplevel(root)
     start_window.title("Invio Temperature Giornaliere – Stazione Meteo")
     label = tk.Label(start_window, text="Per quanti giorni vuoi inserire i dati?")
     label.pack(pady=5)
     entry = tk.Entry(start_window)
     entry.pack(pady=5)
-    button = tk.Button(start_window, text="Conferma", command=getNumDays)
+    button = tk.Button(start_window, text="Conferma", command=lambda: getNumDays(start_window, entry))
     button.pack(pady=10)
     start_window.geometry("500x100")
+
+
+def rootInit():
     root.geometry("300x250")
     label1 = tk.Label(root, text="Inserisci il giorno di rilevazione (GG/MM/AAAA)")
     label1.pack(pady=5)
@@ -81,9 +95,34 @@ if __name__ == "__main__":
     label3.pack(pady=5)
     entry3 = tk.Entry(root)
     entry3.pack(pady=5)
-    button1 = tk.Button(root, text="Invia dati", command=mainInterfaceLogic)
+    button1 = tk.Button(root, text="Invia dati", command=lambda: mainInterfaceLogic(entry1, entry2, entry3))
     button1.pack(pady=10)
+
+
+def resultWindowInit(midTemp, maxTemp, minTemp):  # Da rendere max, min, e med temp come argomenti ricevuti dal server
+    resultWindow = tk.Toplevel(root)
+    resultWindow.title("Risultati")
+    label = tk.Label(resultWindow, text=f"Numero giorni analizzati: {nGiorni}")
+    label.pack(pady=5)
+    label1 = tk.Label(resultWindow, text=f"Numero rilevazioni fatte: {nGiorni * 2}")
+    label1.pack(pady=5)
+    label1 = tk.Label(resultWindow, text=f"Temperatura media: {midTemp}")
+    label1.pack(pady=5)
+    label1 = tk.Label(resultWindow, text=f"Temperatura massima: {maxTemp}")
+    label1.pack(pady=5)
+    label1 = tk.Label(resultWindow, text=f"Temperatura minima: {minTemp}")
+    label1.pack(pady=5)
+    return resultWindow
+
+
+# Chiamata della funzione 'start_client' in main attraverso il 'button' di Tkinter
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+    startWindowInit()
+    rootInit()
+    result_window = resultWindowInit(10, 10, 10)
+    result_window.withdraw()
     root.mainloop()
 
-# Da aggiungere funzionamento con for loop per visualizzare la GUI il numero
-# di giorni necessario e salvare i dati formattati come message nella lista daysData(ancora da dichiarare come lista)
+# Da fixare chiusura istantanea prima interfaccia GUI dopo pressione bottone
